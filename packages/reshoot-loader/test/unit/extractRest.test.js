b6e2d40ce49298ae7c2551d335125847485579ee
Billy Kwok <xobkwok@gmail.com>
2019-03-11 00:26:38 +0800

    fix(@reshoot/loader): fix stringify error

--- packages/reshoot-loader/test/unit/extractRest.test.js
+++ packages/reshoot-loader/test/unit/extractRest.test.js
@@ -4,7 +4,7 @@ import extractRest from '../../src/extractRest';
 describe('extractRest', () => {
   test('extra attributes not in options', () => {
     expect(extractRest({ srcSet: [1, 2, 3], maxWidth: 10 })).toEqual({
-      maxWidth: 10
+      maxWidth: '10'
     });
   });
 });
