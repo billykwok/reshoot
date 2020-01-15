1b861e1fbfc768a818703bdc37d3c59216d7408d
Billy Kwok <xobkwok@gmail.com>
2020-01-15 21:28:28 +0800

    fix: remove unnecessary code

--- packages/reshoot-react/src/useIntersection.ts
+++ packages/reshoot-react/src/useIntersection.ts
@@ -7,7 +7,7 @@ const observer =
   typeof IntersectionObserver !== 'undefined' &&
   new IntersectionObserver(
     entries => entries.forEach(entry => handlers.get(entry.target)(entry)),
-    { root: null, rootMargin: '-5% 0%' }
+    { rootMargin: '-5% 0%' }
   );
 
 export const subscribe = (
