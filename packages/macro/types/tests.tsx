78f8f378fbd178334a798f8f1c0a4464873e9250
Billy Kwok <xobkwok@gmail.com>
2020-02-20 23:30:46 +0800

    fix: fix incorrect type in macro

--- packages/macro/types/tests.tsx
+++ packages/macro/types/tests.tsx
@@ -1,9 +1,35 @@
 import reshoot from '@reshoot/macro';
 
-const data = reshoot('../../img/abc.png', {
+const data1 = reshoot('../../img/abc.png', {
   src: '',
   alt: '',
   aspectRatio: 1.4,
+  blur: 5,
+  color: '',
+  placeholder: '',
+  srcSet: '',
+  extra: 123
+});
+
+export const d1: string = data1.src;
+export const d2: string = data1.alt;
+export const d3: number = data1.aspectRatio;
+export const d4: number = data1.blur;
+export const d5: string = data1.color;
+export const d6: string = data1.placeholder;
+export const d7: string = data1.srcSet;
+export const d8: number = data1.extra;
+
+export const e1: string = data1[0].src;
+export const e2: string = data1[0].alt;
+export const e3: number = data1[0].aspectRatio;
+export const e4: number = data1[0].blur;
+export const e5: string = data1[0].color;
+export const e6: string = data1[0].placeholder;
+export const e7: string = data1[0].srcSet;
+export const e8: number = data1[0].extra;
+
+const data2 = reshoot('../../img/abc.png', {
   blur: 5,
   color: '',
   placeholder: '',
@@ -15,22 +41,4 @@ function Img(props: { src: string; srcSet: string; aspectRatio: number }) {
   return props;
 }
 
-Img({ ...data });
-
-export const d1: string = data.src;
-export const d2: string = data.alt;
-export const d3: number = data.aspectRatio;
-export const d4: number = data.blur;
-export const d5: string = data.color;
-export const d6: string = data.placeholder;
-export const d7: string = data.srcSet;
-export const d8: number = data.extra;
-
-export const e1: string = data[0].src;
-export const e2: string = data[0].alt;
-export const e3: number = data[0].aspectRatio;
-export const e4: number = data[0].blur;
-export const e5: string = data[0].color;
-export const e6: string = data[0].placeholder;
-export const e7: string = data[0].srcSet;
-export const e8: number = data[0].extra;
+Img(data2);
