cca6a277b62622ba0f7917099e67c04a9ef62d75
Billy Kwok <xobkwok@gmail.com>
2020-08-02 18:36:01 +0800

    fix: fix typing of @reshoot/macro

--- packages/macro/src/macro.ts
+++ packages/macro/src/macro.ts
@@ -2,4 +2,51 @@ import { createMacro } from 'babel-plugin-macros';
 
 import handle from './handle';
 
-export default createMacro(handle);
+export default createMacro(handle) as (
+  path: string,
+  options: Readonly<
+    Partial<{
+      name: string;
+      outputPath: string | ((filename: string) => string);
+      publicPath: string | ((filename: string) => string);
+      shape: {
+        mime: string | boolean;
+        src: string | boolean;
+        width: string | boolean;
+        height: string | boolean;
+        srcSet: string | boolean;
+        aspectRatio: string | boolean;
+        placeholder: string | boolean;
+        color: string | boolean;
+      };
+      srcSet: number[];
+      quality: number;
+      background: string;
+      color: string;
+      enforceFormat: string | false;
+      placeholder: { size: number; trimDataUrl: boolean };
+      aspectRatio: {
+        type: 'widthByHeight' | 'heightByWidth';
+        format: 'ratio' | 'percent';
+        decimal: number;
+      };
+      disable: boolean;
+      cache: boolean;
+      emitFile: boolean;
+      esModule: boolean;
+      [key: string]: unknown;
+    }>
+  >
+) => Readonly<
+  Partial<{
+    src: string;
+    width: number;
+    height: number;
+    aspectRatio: number;
+    srcSet: string;
+    alt: string;
+    color: string;
+    placeholder: string;
+    [key: string]: unknown;
+  }>
+>;
