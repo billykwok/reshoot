1605f5b81c54e85a0c58e72583336c56fc494e70
Billy Kwok <xobkwok@gmail.com>
2020-08-16 18:42:57 +0800

    feat: update storybook and other dependencies (#204)

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -6,29 +6,30 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-viewport/register',
     'storybook-dark-mode/register',
-    '@storybook/addon-knobs/register',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-controls',
     '@storybook/addon-actions/register',
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
   webpackFinal: async (config) => {
     const localModules = path.resolve(__dirname, '../../../packages');
     config.resolve.modules.unshift(localModules);
     config.resolve.alias['@reshoot'] = localModules;
     config.resolve.extensions.push('.ts', '.tsx', '.md', '.mdx');
-    config.module.rules.push(
-      {
-        test: /\.[tj]sx?$/,
-        exclude: /node_modules/,
-        enforce: 'pre',
-        use: [
-          { loader: 'babel-loader' },
-          {
-            loader: '@storybook/source-loader',
-            options: { parser: 'typescript' },
-          },
-        ],
-      },
-      { test: /\.md$/, exclude: /node_modules/, loader: 'markdown-loader' }
-    );
+    config.module.rules.push({
+      test: /\.md$/,
+      exclude: /node_modules/,
+      loader: 'markdown-loader',
+    });
     return config;
   },
 };
